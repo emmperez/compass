529c1996bce452d0cfd6aa79fbc8400797be01c9
emmanuel <emmanueleperez@icloud.com>
2025-02-19 21:26:07 -0500

    added test & footer to blog posts

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,6 +3,8 @@ import { blogArticle } from "@/app/lib/interface";
 import Image from "next/image";
 import { urlFor } from "@/app/lib/sanity";
 import { PortableText } from "next-sanity";
+import Testimonials from "@/app/components/Testimonials/Testimonials";
+import Footer from "@/app/components/Footer/Footer";    
 
 export const revalidate = 30;
 
@@ -25,25 +27,29 @@ export default async function BlogArticle({
 }) {
     const data: blogArticle = await getData(params.slug);
     return (
-        <div className="my-5 w-full">
-            <h1>
-                <span className="block text-base text-center text-primary font-semibold tracking-wide">
+        <>
+            <div className="my-5 w-full">
+                <h1>
+                    <span className="block text-base text-center text-primary font-semibold tracking-wide">
 
-                </span>
-                <span className="mt-2 block text-3xl text-center text-primary font-bold tracking-tight">
-                    {data.title}
-                </span>
-            </h1>
-            <Image 
-                src={urlFor(data.titleImage).url()} 
-                alt="Image" 
-                width={1080} 
-                height={800}
-                className="w-full max-h-[600px] rounded-lg object-cover mt-8 border" 
-            />
-            <div className="mt-16 prose prose-md mx-auto">
-                <PortableText value={data.content} />
+                    </span>
+                    <span className="mt-2 block text-3xl text-center text-primary font-bold tracking-tight">
+                        {data.title}
+                    </span>
+                </h1>
+                <Image 
+                    src={urlFor(data.titleImage).url()} 
+                    alt="Image" 
+                    width={1080} 
+                    height={800}
+                    className="w-full max-h-[600px] rounded-lg object-cover mt-8 border" 
+                />
+                <div className="mt-16 prose prose-md mx-auto">
+                    <PortableText value={data.content} />
+                </div>
             </div>
-        </div>
+            <Testimonials />
+            <Footer />
+        </>
     )
 }
