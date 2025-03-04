1897d39f047e6bf1fee67090a90a1f3c994e8950
emmanuel <emmanueleperez@icloud.com>
2025-03-04 07:07:57 -0500

    added parallax image animation

--- app/components/Promo/PromoClient.tsx
+++ app/components/Promo/PromoClient.tsx
@@ -3,13 +3,22 @@ import Image from "next/image";
 import { urlFor } from "@/app/lib/sanity";
 import Link from "next/link";
 import { Promo } from "@/app/lib/interface";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 interface PromoClientProps {
     promo: Promo[]; 
 }
 
 export default function PromoClient({ promo }: PromoClientProps) { 
+    const ref = useRef(null);
+
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start end", "end start"],
+    });
+
+    const y = useTransform(scrollYProgress, [0, 1], [-350, 50]);
 
     if (promo.length === 0) {
         return <p>No promo articles available.</p>;
@@ -19,16 +28,20 @@ export default function PromoClient({ promo }: PromoClientProps) {
         <section className="hero relative h-[75vh] w-full overflow-hidden rounded-xl mb-8">
             <Link href={`/blog/${promo[0].currentSlug}`}>
                 <div className="absolute inset-0 overflow-hidden">
-                    <div className="relative h-[100vh] w-full transform translate-y-[-10%]">
+                    <motion.div 
+                        ref={ref}
+                        style={{ y }}
+                        className="relative h-[100vh] w-full transform translate-y-[-10%]">
                         <Image
                             src={urlFor(promo[0].titleImage.asset).url() || ''}
-                            width={1920}
-                            height={1280}
+                            // width={1920}
+                            // height={1280}
+                            fill
                             alt="Five women standing on seashore"
                             priority={true}
                             className="absolute inset-0 h-full w-full object-cover scale-110 motion-safe:animate-parallax"
                         />
-                    </div>
+                    </motion.div>
                 </div>
                 <div className="absolute inset-0 bg-black/20" />
                 <div className="absolute top-10 px-4 z-10 w-full lg:max-w-[60%] text-white">
